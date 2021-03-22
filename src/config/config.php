b723fdbcc666be49ef32dd71bdae900d7ab9a993
AdrianPrietoLozano <adrian.prieto@alumnos.udg.mx>
2021-03-22 14:35:30 -0600

    Cambios menores

--- src/config/config.php
+++ src/config/config.php
@@ -6,6 +6,6 @@ define('PASSWORD', '');
 define('DBNAME', 'eventos_limpieza');
 define('CLIENT_ID', "192312538883-ttvi1tbb3q79kqruirmgr40r1u781e6g.apps.googleusercontent.com");
 
-define('FCM_KEY', "AAAALMa4dwM:APA91bH3iLqe_7TkpP2weMk6O5qOap0aB1uVZPQyADlgdKg1PwNBc9hOan16s740StnrdaesdFqikphvPv5DxjGg9tkIfKhLD0kIzQl-gHPbZXlzBDKCYckUpHiCZ90GWTxRCItfKXZx")
+define('FCM_KEY', /*reemplazar*/)
 
 ?>
